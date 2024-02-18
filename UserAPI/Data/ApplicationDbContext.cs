95c82fce30cb746467f68876989c126a156e95d6
Kuan Jiun Ying <jiunying2000kjy@gmail.com>
2024-02-18 22:48:27 +0800

    Add project files.

---
+++ UserAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿// Data/ApplicationDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using UserAPI.Models;
+
+namespace UserAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
